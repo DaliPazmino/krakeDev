3df11e1e54fc7dcad1607cc56497684f4e2d082b
DaliPazmino <dalilapazmino@gmail.com>
2025-10-14 22:09:30 -0500

    ejercicios

---
+++ ejercicioGit/ejercicios.js
@@ -0,0 +1,7 @@
+saludar = function() {
+    let cmpImagenSaludo;
+    
+    cmpImagenSaludo = document.getElementById("imgSaludo");
+    cmpImagenSaludo.src = "./imagenes/0fea10ec5cb60a6cb8cf3bfd3213d40e.gif";
+    console.log ("los pollitos dicen");
+}
