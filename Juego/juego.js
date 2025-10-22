5c451506f6d171ae5885c8f267a8159eb7cce493
DaliPazmino <dalilapazmino@gmail.com>
2025-10-22 10:35:00 -0500

    se añade puntaje y diseño sencillo de pagina y los casos de prueba el word

--- Juego/juego.js
+++ Juego/juego.js
@@ -1,3 +1,6 @@
+let puntosUsuario = 0;
+let puntosComputador = 0;
+
 
 jugar = function (seleccionado) {
     // La compu elige
@@ -14,11 +17,34 @@ jugar = function (seleccionado) {
         mostrarTexto("lblResultado", "EMPATE");
     } else if (resultado === 1) {
         mostrarTexto("lblResultado", "¡GANASTE LA PARTIDA!");
-        //puntosUsuario = puntosUsuario + 1;
+        puntosUsuario = puntosUsuario + 1;
     } else {
         mostrarTexto("lblResultado", "PERDISTE LA PARTIDA");
-        //puntosComputador = puntosComputador + 1;
+        puntosComputador = puntosComputador + 1;
     }
+
+    //Mostrar puntajes actualizados
+    mostrarTexto("lblPuntosUsuario", puntosUsuario.toString());
+    mostrarTexto("lblPuntosComputador", puntosComputador.toString());
+
+    if (puntosUsuario === 5) {
+        mostrarTexto("lblResultado", "¡HAS GANADO EL JUEGO!");
+        deshabilitarBotones();
+    } else if (puntosComputador === 5) {
+        mostrarTexto("lblResultado", "EL COMPUTADOR TE HA VENCIDO");
+        deshabilitarBotones();
+    }
+
+    nuevaPartida = function () {
+        puntosUsuario = 0;
+        puntosComputador = 0;
+        mostrarTexto("lblPuntosUsuario", "0");
+        mostrarTexto("lblPuntosComputador", "0");
+        mostrarTexto("lblResultado", "");
+        cambiarImagen("imgComputador", "");
+        habilitarBotones();
+    }
+
 }
 
 
