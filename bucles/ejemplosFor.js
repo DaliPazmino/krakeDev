49e5081de0346f16252a7de6f46f62b9b1b0e7b4
DaliPazmino <dalilapazmino@gmail.com>
2025-10-22 11:20:13 -0500

    buble for impares

---
+++ bucles/ejemplosFor.js
@@ -0,0 +1,58 @@
+mostrarNumeros = function (){
+    console.log ("antes del for");
+
+    for (let i=0; i<4; i++){
+        console.log(i);
+    }
+
+    console.log("despues del for")
+    
+}
+mostrarNumeros2 = function (){
+    console.log ("antes del for");
+
+    for (let indice=1; indice<=5; indice++){
+        console.log(indice);
+    }
+
+    console.log("despues del for")
+    
+}
+
+mosstrarPares = function (){
+    console.log ("antes del for");
+
+    for (let indice=0; indice<=10; indice+=2){
+        console.log(indice);
+    }
+
+    console.log("despues del for")
+    
+}
+mosstrarInverso = function (){
+        console.log ("antes del for");
+
+    for (let indice=10; indice>=0; indice--){
+        console.log(indice);
+    }
+
+    console.log("despues del for")
+    
+}
+
+hackearNasaPelis = function (){
+    for (let porcentaje=0; porcentaje <=100; porcentaje+=10){
+        console.log("hackeando nasa "+ porcentaje+ "%");
+    }
+      console.log ("la nasa ha sido hackeada xd");
+}
+
+mostrarImpares = function (){
+   console.log ("antes del for");
+
+    for (let i=1; i<=21; i+=2){
+        console.log(i);
+    }
+
+    console.log("despues del for")
+}
